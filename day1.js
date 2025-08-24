2d0f967cd5f500ec192325b836f3a6608b96acfd
Ruhullah Rep <ruhullah4u@gmail.com>
2025-08-24 10:56:49 +0530

    my Javascript program

---
+++ day1.js
@@ -0,0 +1,11 @@
+let Personname = "Ruhullah";
+Personname = "Nayan"
+let company = "Wipro";
+let Designation = "Architect";
+let skills = ["java", "Appium","TypeScript"]
+const salary = 250000
+console.log(`My name is ${Personname}, I work as an ${Designation},my company name is ${company},my salary ${salary}, my skills ${skills}.`);
+console.log("Bismillah My FirstPogram");
+//updated salary
+const salary1 = 250010
+console.log(`updated salary ${salary1}`);
